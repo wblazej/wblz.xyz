6fb0303c93a8097c39bc6902f1b9fcbe4564f8a8
wblazej <blazej.wrzosok@gmail.com>
2022-01-19 16:35:22 +0100

    tech stack card

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -1,5 +1,21 @@
-import CSSLogo from '../svg/tech/CSSLogo';
 import './../style/slides.scss';
+
+import CSSLogo from '../svg/tech/CSSLogo';
+import HTMLLogo from '../svg/tech/HTMLLogo';
+import ReactLogo from '../svg/tech/ReactLogo';
+import JavaScriptLogo from '../svg/tech/JavaScriptLogo';
+import TypeScriptLogo from '../svg/tech/TypeScriptLogo';
+import PythonLogo from '../svg/tech/PythonLogo';
+import FlaskLogo from '../svg/tech/FlaskLogo';
+import MySQLLogo from '../svg/tech/MySQLLogo';
+import PostgreSQLLogo from '../svg/tech/PostgreSQLLogo';
+import RedisLogo from '../svg/tech/RedisLogo';
+import GitLogo from '../svg/tech/GitLogo';
+import DockerLogo from '../svg/tech/DockerLogo';
+import RubyLogo from '../svg/tech/RubyLogo';
+import RailsLogo from '../svg/tech/RailsLogo';
+import GoogleCloudLogo from '../svg/tech/GoogleCloudLogo';
+import CPlusPlusLogo from '../svg/tech/CPlusPlusLogo';
 
 const Slides = () => {
     return (
@@ -7,13 +23,29 @@ const Slides = () => {
 			<ul id="cards">
                 <li className="card">
                     <div className="content">
-                        <h1>tech stack</h1>
-                        <p className="under-header">Technologies I'm familiar with</p>
-
-                        <div className="tech-stack-container">
-                            <div className="technologies">
-                                <div className="item"><CSSLogo /></div>
+                        <div className="header-panel" id="tech-stack">
+                            <div className="header">
+                                <h2>tech stack</h2>
+                                <p>technologies I'm familiar with</p>
                             </div>
+                        </div>
+                        <div className="card-content">
+                            <div className="item"><CSSLogo /></div>
+                            <div className="item"><HTMLLogo /></div>
+                            <div className="item"><ReactLogo /></div>
+                            <div className="item"><JavaScriptLogo /></div>
+                            <div className="item"><TypeScriptLogo /></div>
+                            <div className="item"><PythonLogo /></div>
+                            <div className="item"><FlaskLogo /></div>
+                            <div className="item"><MySQLLogo /></div>
+                            <div className="item"><PostgreSQLLogo /></div>
+                            <div className="item"><RedisLogo /></div>
+                            <div className="item"><GitLogo /></div>
+                            <div className="item"><DockerLogo /></div>
+                            <div className="item"><RubyLogo /></div>
+                            <div className="item"><RailsLogo /></div>
+                            <div className="item"><GoogleCloudLogo /></div>
+                            <div className="item"><CPlusPlusLogo /></div>
                         </div>
                     </div>
                 </li>
---
+++ src/svg/tech/RubyLogo.tsx
@@ -0,0 +1,127 @@
+const RubyLogo = () =>
+    <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_23_109)">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M26.168 128.112L121.137 121.667L128.451 25.9062L99.5139 84.5595L26.168 128.112V128.112Z" fill="url(#paint0_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M90.8967 94.6225L121.293 121.602L113.13 65.2633L90.8967 94.6225V94.6225Z" fill="url(#paint1_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M26.4856 127.989L121.403 121.602L61.6025 116.907L26.4856 127.989V127.989Z" fill="url(#paint2_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.6355 86.0896L26.5701 128.002L41.5087 79.0612L8.6355 86.0896V86.0896Z" fill="url(#paint3_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M37.8064 77.8163L90.8901 94.7846L77.1445 40.9423L37.8064 77.8163Z" fill="url(#paint4_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M77.7928 44.8067L125.333 41.7009L88.1475 11.3304L77.7928 44.8067V44.8067Z" fill="url(#paint5_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M72.2749 0.340088L107.943 0.502185L86.0728 12.5883L72.2749 0.340088Z" fill="url(#paint6_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.73655 65.8923L-0.013916 102.507L9.14775 85.7977L1.73655 65.8923Z" fill="url(#paint7_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.2439 65.2633L8.70038 86.4138L41.1002 79.1454L78.091 44.7678L88.5302 11.609L72.0934 0.00292969L44.1478 10.4614C35.3426 18.6508 18.2575 34.8539 17.6417 35.1588C17.0322 35.47 6.35946 55.6413 1.2439 65.2633V65.2633Z" fill="white" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M27.4258 27.2676C46.508 8.34777 71.1079 -2.83062 80.5485 6.69419C89.9824 16.2193 79.9776 39.3666 60.8957 58.2801C41.8135 77.1936 17.5185 88.9881 8.08427 79.4629C-1.35632 69.9448 8.34356 46.1814 27.4258 27.2676Z" fill="url(#paint8_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M26.5701 127.982L41.3922 78.8861L90.6179 94.7001C72.8194 111.39 53.0242 125.499 26.5701 127.982Z" fill="url(#paint9_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M78.156 44.6706L90.793 94.726C105.661 79.0935 119.005 62.2873 125.54 41.4998L78.156 44.6706Z" fill="url(#paint10_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M125.411 41.7528C130.468 26.4897 131.635 4.59358 107.787 0.528168L88.2188 11.3368L125.411 41.7528Z" fill="url(#paint11_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M-0.013916 102.286C0.686079 127.476 18.8606 127.852 26.6023 128.073L8.71971 86.31L-0.013916 102.286V102.286Z" fill="#9E1209" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M78.2272 44.7484C89.6516 51.7702 112.676 65.8727 113.143 66.1323C113.869 66.5407 123.07 50.6162 125.158 41.6167L78.2272 44.7484V44.7484Z" fill="url(#paint12_radial_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M41.3726 78.8861L61.1874 117.115C72.904 110.761 82.0786 103.019 90.4817 94.7261L41.3726 78.8861Z" fill="url(#paint13_radial_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.64186 86.3619L5.83459 119.793C11.1319 127.029 18.4196 127.658 26.0642 127.094C20.5335 113.329 9.48495 85.8044 8.64186 86.3619Z" fill="url(#paint14_linear_23_109)" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M88.1022 11.421L127.466 16.9453C125.365 8.04289 118.914 2.29825 107.917 0.502197L88.1022 11.421V11.421Z" fill="url(#paint15_linear_23_109)" />
+        </g>
+        <defs>
+            <linearGradient id="paint0_linear_23_109" x1="112.851" y1="139.76" x2="85.7488" y2="91.9088" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FB7655" />
+                <stop offset="0.41" stop-color="#E42B1E" />
+                <stop offset="0.99001" stop-color="#990000" />
+                <stop offset="1" stop-color="#990000" />
+            </linearGradient>
+            <linearGradient id="paint1_linear_23_109" x1="126.353" y1="99.5671" x2="91.4253" y2="76.1283" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#871101" />
+                <stop offset="0.99001" stop-color="#911209" />
+                <stop offset="1" stop-color="#911209" />
+            </linearGradient>
+            <linearGradient id="paint2_linear_23_109" x1="98.4074" y1="141.211" x2="63.4794" y2="117.772" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#871101" />
+                <stop offset="0.99001" stop-color="#911209" />
+                <stop offset="1" stop-color="#911209" />
+            </linearGradient>
+            <linearGradient id="paint3_linear_23_109" x1="25.0763" y1="82.6023" x2="30.491" y2="117.792" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" />
+                <stop offset="0.23" stop-color="#E57252" />
+                <stop offset="0.46001" stop-color="#DE3B20" />
+                <stop offset="0.99001" stop-color="#A60003" />
+                <stop offset="1" stop-color="#A60003" />
+            </linearGradient>
+            <linearGradient id="paint4_linear_23_109" x1="62.3173" y1="49.7415" x2="64.3124" y2="85.6558" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" />
+                <stop offset="0.23" stop-color="#E4714E" />
+                <stop offset="0.56" stop-color="#BE1A0D" />
+                <stop offset="0.99001" stop-color="#A80D00" />
+                <stop offset="1" stop-color="#A80D00" />
+            </linearGradient>
+            <linearGradient id="paint5_linear_23_109" x1="95.3668" y1="16.552" x2="101.339" y2="42.2899" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" />
+                <stop offset="0.17999" stop-color="#E46342" />
+                <stop offset="0.39999" stop-color="#C82410" />
+                <stop offset="0.99001" stop-color="#A80D00" />
+                <stop offset="1" stop-color="#A80D00" />
+            </linearGradient>
+            <linearGradient id="paint6_linear_23_109" x1="77.129" y1="7.48693" x2="102.866" y2="-5.38167" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" />
+                <stop offset="0.53999" stop-color="#C81F11" />
+                <stop offset="0.99001" stop-color="#BF0905" />
+                <stop offset="1" stop-color="#BF0905" />
+            </linearGradient>
+            <linearGradient id="paint7_linear_23_109" x1="2.51727" y1="73.6309" x2="4.63558" y2="94.8397" gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" />
+                <stop offset="0.31" stop-color="#DE4024" />
+                <stop offset="0.99001" stop-color="#BF190B" />
+                <stop offset="1" stop-color="#BF190B" />
+            </linearGradient>
+            <linearGradient id="paint8_linear_23_109" x1="-12.0484" y1="100.574" x2="87.5275" y2="-1.8176" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#BD0012" />
+                <stop offset="0.07001" stop-color="white" />
+                <stop offset="0.27" stop-color="#C82F1C" />
+                <stop offset="0.33" stop-color="#820C01" />
+                <stop offset="0.46001" stop-color="#A31601" />
+                <stop offset="0.72" stop-color="#B31301" />
+                <stop offset="0.99001" stop-color="#E82609" />
+                <stop offset="1" stop-color="#E82609" />
+            </linearGradient>
+            <linearGradient id="paint9_linear_23_109" x1="64.2264" y1="110.899" x2="34.2357" y2="103.497" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#8C0C01" />
+                <stop offset="0.53999" stop-color="#990C00" />
+                <stop offset="0.99001" stop-color="#A80D0E" />
+                <stop offset="1" stop-color="#A80D0E" />
+            </linearGradient>
+            <linearGradient id="paint10_linear_23_109" x1="115.74" y1="74.9008" x2="89.0953" y2="51.0198" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#7E110B" />
+                <stop offset="0.99001" stop-color="#9E0C00" />
+                <stop offset="1" stop-color="#9E0C00" />
+            </linearGradient>
+            <linearGradient id="paint11_linear_23_109" x1="125.529" y1="31.0845" x2="112.256" y2="16.8951" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#79130D" />
+                <stop offset="0.99001" stop-color="#9E120B" />
+                <stop offset="1" stop-color="#9E120B" />
+            </linearGradient>
+            <radialGradient id="paint12_radial_23_109" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(93.2437 51.4786) scale(32.6529)">
+                <stop stop-color="#A80D00" />
+                <stop offset="0.99001" stop-color="#7E0E08" />
+                <stop offset="1" stop-color="#7E0E08" />
+            </radialGradient>
+            <radialGradient id="paint13_radial_23_109" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(48.0252 94.5057) scale(43.4077)">
+                <stop stop-color="#A30C00" />
+                <stop offset="0.99001" stop-color="#800E08" />
+                <stop offset="1" stop-color="#800E08" />
+            </radialGradient>
+            <linearGradient id="paint14_linear_23_109" x1="17.2792" y1="127.955" x2="6.46275" y2="91.2585" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#8B2114" />
+                <stop offset="0.42999" stop-color="#9E100A" />
+                <stop offset="0.99001" stop-color="#B3100C" />
+                <stop offset="1" stop-color="#B3100C" />
+            </linearGradient>
+            <linearGradient id="paint15_linear_23_109" x1="100.255" y1="6.35607" x2="124.504" y2="17.0603" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#B31000" />
+                <stop offset="0.44" stop-color="#910F08" />
+                <stop offset="0.99001" stop-color="#791C12" />
+                <stop offset="1" stop-color="#791C12" />
+            </linearGradient>
+            <clipPath id="clip0_23_109">
+                <rect width="128" height="128" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+
+export default RubyLogo;
