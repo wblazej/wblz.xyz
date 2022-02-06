b43ce6322c4270704d3d4a251315870b7361bca8
wblazej <blazej.wrzosok@gmail.com>
2022-02-06 21:30:38 +0100

    about page

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import './style/index.scss';
 import Home from './elements/Home/Home';
 import NavBar from './elements/NavBar/NavBar';
 import Socials from './elements/Socials/Socials';
+import About from './elements/About/About';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<NavBar/>
 		<Socials/>
 		<Home/>
+		<About/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
