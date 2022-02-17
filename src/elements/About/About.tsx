a50f3a141ad42287678af74cc2281739213146fa
wblazej <blazej.wrzosok@gmail.com>
2022-02-17 19:11:21 +0100

    about section update

--- src/elements/About/About.tsx
+++ src/elements/About/About.tsx
@@ -10,13 +10,13 @@ const About = () => {
                 <p>Hey, my name is Błażej and I'm a full stack software developer and cybersecurity researcher from Poland. Currently, I'm a 17 years old high school student. I really enjoy my job and try to do my best every single day.</p>
 
                 <h4>Tech Stack</h4>
-                <p>HTML, CSS, JavaScript, TypeScript, Python, SQL, Ruby, C++</p>
+                <p>HTML, CSS, JavaScript, TypeScript, Python, Ruby, SQL, C++</p>
 
                 <h4>Libraries & Frameworks</h4>
                 <p>React.js, Sass, Node.js, Flask, Ruby on Rails</p>
 
                 <h4>Tools & Platforms</h4>
-                <p>Git, Docker, Google Cloud Platform, GitHub, Figma</p>
+                <p>Git, Docker, Google Cloud Platform, GitHub, Figma, Vercel, Cloudflare</p>
 
                 <h4>Operating System</h4>
                 <p>macOS Monterey 12.1</p>
